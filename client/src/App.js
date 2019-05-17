7ed24de4db6b9aceb2a6b952091df1ee46fcb3ef
Emmanuel Akhere Iyere <iyereemmanuelakhere@gmail.com>
2019-05-17 08:19:21 +0200

    initiated apollo client

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="space-x"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
